0da76f34d8e75ef07eac9dd39c9bbe5c7d587a70
Cesar Aguilar <Blueavian9@gmail.com>
2024-09-06 14:56:35 -0700

    updated paths

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 // import "./components/Contact.jsx";
-import "./index.css"; // Assuming this is where your Tailwind CSS is set up
+import "./main.css"; // Assuming this is where your Tailwind CSS is set up
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+document.body.innerHTML = '<div id="app"></div>';
+
+// Render your React;
