b63c46688501c8e3a0bb5e2a271690bd6b2ea792
Cesar Aguilar <Blueavian9@gmail.com>
2024-09-06 10:36:55 -0700

    updated tailwind imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
 import "./App.css";
 import "./main.jsx";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import GlowButton from "./components/GlowButton";
 import About from "./components/About";
+import Service from "./components/Service";
+import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-import GlowButton from "./components/GlowButton";
-import Hero from "./components/Hero";
-import Navbar from "./components/Navbar";
-import Projects from "./components/Projects";
-import Service from "./components/Service";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <GlowButton />
       <About />
       <Service />
       <Projects />
       <Contact />
       <Footer />
-      <GlowButton />
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
-import "./components/Contact.jsx";
+// import "./components/Contact.jsx";
+import "./index.css"; // Assuming this is where your Tailwind CSS is set up
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
