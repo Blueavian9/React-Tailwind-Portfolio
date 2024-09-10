0ae30014ff3a552ee2468da8fa5343cd0940eb56
Cesar Aguilar <Blueavian9@gmail.com>
2024-09-10 13:18:27 -0700

    updated assets images

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,13 +5,13 @@ const About = () => {
   return (
     <div className="bg-black text-white py-20" id="about">
       <div className="container mx-auto px-8 md:px-16 lg:px-24">
-        <h2 className="text-4xl font-bold text-center mb-12">About Me</h2>
         <div className="flex flex-col md:flex-row items-center md:space-x-12">
+        <h2 className="text-4xl font-bold text-center mb-12">About Me</h2>
           <img
             src={CesarCover2Img}
             alt="CesarCover2"
             className="w-72 h-80 rounded object-cover mb-8 md:mb-0"
-          />
+            />
           <div className="flex-1">
             <p className="text-lg mb-8">
               {/* I am a passionate full-stack developer with a focus on building
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,11 @@ import * as React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="bg-black text-white px-8 md:px-16 lg:px-24">
+    <nav className="bg-black text-cyan-400 px-8 md:px-16 lg:px-24">
       <div className="container py-2 flex justify-center md:justify-between items-center">
-        <div className="text-2xl font-bold hidden md:inline">Cesar Adrian Aguilar</div>
+        <div className="text-2xl font-bold hidden md:inline">
+          Cesar Adrian Aguilar
+        </div>
         <div className="space-x-6">
           <a href="#home" className="hover:text-gray-400">
             Home
