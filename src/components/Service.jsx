5e1469e3ed2339513f9fa0f67b1f73126c5c8cb1
Cesar Aguilar <Blueavian9@gmail.com>
2024-09-18 09:19:33 -0700

    updated services

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -45,7 +45,7 @@ const Service = () => {
       <div className="container mx-auto px-8 md:px-16 lg:px-24">
         <h2 className="text-4xl font-bold text-center mb-12">
           <span className="text-transparent bg-clip-text bg-gradient-to-r from-teal-200 to-blue-500">
-            My Services
+            Services
           </span>
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
