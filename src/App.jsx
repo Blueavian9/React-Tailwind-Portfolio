62db8606fe8a679d04e5e5935d1ae0d8b464d107
Cesar Aguilar <Blueavian9@gmail.com>
2025-04-08 13:40:33 -0700

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ import * as React from "react";
 import Navbar from "../src/components/Navbar.jsx";
 import Hero from "../src/components/Hero.jsx";
 import About from "../src/components/About.jsx";
-import Service from "../src/components/Service.jsx";
+import Services from "./components/Services.jsx";
 import Projects from "../src/components/Projects.jsx";
 import Contact from "../src/components/Contact.jsx";
 import Footer from "../src/components/Footer.jsx";
-
+import
 export default function App() {
   return (
     <div>
