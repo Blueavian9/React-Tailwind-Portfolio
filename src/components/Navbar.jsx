6d855504e8852aa0e804e6b4d19ec87822ccde37
Cesar Aguilar <Blueavian9@gmail.com>
2025-04-14 11:49:49 -0700

    updated Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,12 +6,13 @@ const Navbar = () => {
       <div className="container mx-auto flex justify-center items-center border-round">
         <div className="space-x-8">
           <div className="text-2xl font-bold">
+            {/*
             <a href="#home" className="hover:text-cyan-400"></a>
           </div>
-          <div className="space-x-8">
+           <div className="space-x-8">
             <a href="#home" className="hover:text-cyan-400">
               <strong>Home</strong>
-            </a>
+            </a> */}
             <a href="#about" className="hover:text-cyan-400">
               <strong>About</strong>
             </a>
